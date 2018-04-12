eaadade85312cc249ce898021cb83fb5eb20a92d
Julio Mauro <juliomauro@gmail.com>
2018-04-12 15:15:38 -0300

    upload

--- GetRoute53Record.sh
+++ GetRoute53Record.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/sh
+## Author: Julio Cesar Mauro
+## Date: 12-04-2018
+## Script to show DNS records from AWS - Route53
 
 if [ "$1" == "" ]; then
 echo "Domain was not informed"
@@ -7,6 +10,7 @@ echo "usage: GetRoute53Record <DOMAIN>"
 echo ""
 exit 1
 fi
+
 zonename=$1
 hostedzoneid=$(aws route53 list-hosted-zones | jq -r ".HostedZones[] | select(.Name == \"$zonename.\") | .Id" | cut -d'/' -f3)
 aws route53 list-resource-record-sets --hosted-zone-id $hostedzoneid --output json | jq -jr '.ResourceRecordSets[] | "\(.Name) \t\(.TTL) \t\(.Type) \t\(.ResourceRecords[].Value)\n"'
